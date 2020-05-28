713ea73524286ff22f3ef98284f57639773571e2
elenaguilarv <aplerc@hotmail.com>
2020-05-28 14:26:03 -0700

    first commit

---
+++ PyPoll.py
@@ -0,0 +1,35 @@
+# The data we need to retrieve.
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote
+
+
+# Add our dependencies.
+import csv
+import os
+
+# Assign a variable for the file to load file from a path
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+with open(file_to_save,"w") as txt_file:
+    # Write some data to the file
+    txt_file.write("hELLO World!")
+    # Add more modifications
+    txt_file.write("\nCounties in the Election\n--------------------------")
+    # Write three countries to the file
+    txt_file.write(" \nArapahoe\nDenver\nJefferson") 
+
+# Open election results file and read it
+with open(file_to_load) as election_data:
+    
+    # To do: Read and analyze data here
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Read and print header row. 
+    headers = next(file_reader)
+    print(headers)
---
+++ Python-practice.py
@@ -0,0 +1,7 @@
+print("Hello World")
+print(type(3))
+counties = ["Arapahoe","Denver","Jefferson"]
+print(counties)
+print(counties[0:1])
+counties.append("El Paso")
+print(counties)
